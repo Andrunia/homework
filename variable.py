2996c0b973ea832c01bf3e85d1738da12279fb44
Andreyka <druniiiiaa@gmail.com>
2025-01-11 22:33:13 +0500

    Add files via upload
    11|01 Переменные

---
+++ variable.py
@@ -0,0 +1,6 @@
+# Переменные
+col_dz = 12
+hours = 1.5
+lesson_name = "Python"
+sr_time = hours / col_dz
+print ('Курс:',lesson_name,"всего задач: ", col_dz,"затрачено часов: ", hours,'среднее время выполнения: ', sr_time, 'часа' )
