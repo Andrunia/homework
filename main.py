d7c83758848ceac210fe30228b2852671c7b0571
Andreyka <druniiiiaa@gmail.com>
2025-01-11 21:53:57 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+# 1st program
+print(9 ** 0.5 * 5)
+
+# 2nd program
+print (9.99 > 9.98 and 1000 != 1000.1)
+
+# 3rd program
+print (2 * 2 + 2)
+print (2 * (2 + 2))
+a = 2 * 2 + 2
+b = 2 * (2 + 2)
+print (a == b )
+
+# 4th program
+c = "123.456"
+c = float(c)
+b = c * 10 % 10
+print (int(b))
