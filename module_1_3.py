ee6e13adb597d4b1935ef23d94cf7aff734ea9e7
Andreyka <druniiiiaa@gmail.com>
2025-01-11 22:17:39 +0500

    Add files via upload
    11|01 Динамическая типизация

---
+++ module_1_3.py
@@ -0,0 +1,9 @@
+# Динамическая типизация
+name = "Andrew"
+print ("Name:",name)
+age = 22
+print ("Age:", age)
+age += 10
+print ('New age:', age)
+is_student = True
+print("Is_student:", is_student)
