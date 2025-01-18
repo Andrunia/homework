1bd007af9e2553be9ad0042107bc28397e805fdd
Andreyka <druniiiiaa@gmail.com>
2025-01-18 16:34:14 +0500

    Add files via upload
    18|01 Функции в Python.Функция с параметром

---
+++ module_2_5.py
@@ -0,0 +1,19 @@
+# 18|01 Функции в Python.Функция с параметром
+
+def get_matrix(n, m, value):
+    matrix = []
+    for i in range(n):
+        list = []
+        matrix.append(list)
+        for j in range(m):
+            if value <= 0: return matrix
+            list.append(value)
+    return matrix
+
+
+result1 = get_matrix(2, 2, 10)
+result2 = get_matrix(3, 5, 42)
+result3 = get_matrix(4, 2, 13)
+print(result1)
+print(result2)
+print(result3)
