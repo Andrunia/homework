eaa8db1c33b8c4871a08c49da083dee3143dac3d
Andreyka <druniiiiaa@gmail.com>
2025-01-25 18:00:39 +0500

    Update module_1_6.py

--- module_1_6.py
+++ module_1_6.py
@@ -1,12 +1,12 @@
 # Словари и множества
 my_dict = {'Имя': "Андрей", 'Год рождения': 2002}
-print(my_dict)
-print(my_dict['Имя'])
+print(f"Dict: {my_dict}")
+print(f"Existing item: {my_dict['Год рождения']}")
+print(f"Not existing value: {my_dict.get("Никнейм")}")
 my_dict.update({'Возраст': 22, 'Пол': 'мужской'})
-print(my_dict)
 del my_dict['Год рождения']
-print(my_dict)
-print("-"*10)
+print(f"Deleted value: {my_dict.setdefault('Год рождения',2002)}")
+print()
 my_set = {1, 2, 2, 3, "kak", "kak", False, (1, 3, "str")}
 print(my_set)
 my_set.add(5)
